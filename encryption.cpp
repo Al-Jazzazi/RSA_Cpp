9ae51a9f3b0b57e2ef1e6f5b5c3abe813edbe8ed
Al-Jazzazi <ya2225@nyu.edu>
2024-11-12 14:51:28 -0500

    finished encryption

--- encryption.cpp
+++ encryption.cpp
@@ -1,7 +1,8 @@
 #include <Security/Security.h>
 #include <iostream>
 #include <vector>
-
+#include <cmath> 
+#include <string>
 unsigned long long generate_secure_random_bytes(size_t length =8) {
     std::vector<unsigned char> buffer(length);
     if (SecRandomCopyBytes(kSecRandomDefault, length, buffer.data()) != errSecSuccess) {
@@ -13,7 +14,7 @@ unsigned long long generate_secure_random_bytes(size_t length =8) {
         number |= (static_cast<unsigned long long>(buffer[i]) << (i * 8));
         // std::cout << std::hex << (int)buffer[i] << " "; 
     }
-
+       
     return number;
 }
 
@@ -29,34 +30,107 @@ bool isPrime(unsigned long long num) {
     
     return true;
 }
+ 
+unsigned long long gcd(unsigned long long a, unsigned long long b) {
+    if (a == 0 || b == 0) {
+        return a;
+    }
+    if (a == b)  
+        return a;  
+
+    return gcd(b, a % b);
+}
+
+bool areCoprime(unsigned long long a, unsigned long long b) {
+    return gcd(a, b) == 1;
+}
 
 
-int main() {
-    size_t length = 8; 
+unsigned long long find_d(unsigned long long e, unsigned long long phi){
+    unsigned long long d = 1;
+
+
+    return d; 
+}
+
+
+unsigned long long mod_exp(int base, unsigned long long exp, unsigned long long mod){
+    unsigned long long result =1 ; 
+    int power = 2;
+    unsigned long long prev = base % mod;
+    do{
+        if( (exp&1) == 1){
+            
+            result *= prev;
+
+        }
+        exp= exp >> 1;
+        prev = (unsigned long long)pow(prev, power) % mod;
+
+
+    }while(exp > 0);
+
+    return (result%mod);
+
+}
+
+
+std::vector<unsigned long long> encrypt(std::string str,unsigned long long e, unsigned long long n){
+    std::vector<unsigned long long > c; 
+    for(auto i = str.begin(); i != str.end(); i++){
+
+        c.push_back(mod_exp(*i, e, n));
+       
+    }
+    return c; 
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+    size_t length = 4; 
     auto p = generate_secure_random_bytes(length);
 
     while(!isPrime(p)){
         p = generate_secure_random_bytes(length);
     }
-    std::cout <<p << " ";
+    // std::cout <<p << " ";
 
     auto q = generate_secure_random_bytes(length);
-    while(!isPrime(q)){
+    while(!isPrime(q) || p==q){
         q = generate_secure_random_bytes(length);
     }
 
-    std::cout <<q << " ";
+    // std::cout <<q << " ";
 
-    unsigned long long n = p*q; 
-   
-    std::cout <<q << " ";
-
+    unsigned long long n = (p*q); 
+    unsigned long long phi = (p-1)*(q-1);
     
+    // std::cout <<n << " " << phi << "\n";
 
+
+    unsigned long long e =11;
+    while(!areCoprime(phi,e) || e > phi ){
+        e  = generate_secure_random_bytes(length/2);
+    }
+
+    std::cout << "Public key in format(e, n) is (" << e  << ", " << n << ")\n"; 
+
+
+    for(int i =1; i< argc; i++){
+        auto c = encrypt(argv[i], e, n);
+        std::cout << "Encryption of \"" << argv[i] << "\" is: ";
+        for(auto l: c){
+            std::cout <<std::hex << l << " "; 
+        }
+        c.clear();
+        std::cout << "\n"; 
     
+    }
 
+ 
 
-    
     std::cout << std::dec << std::endl;
     
  
